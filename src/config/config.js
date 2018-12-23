970fcca829407b7937be68e917b0fd1da6d75f5e
Sean.Rennie <sean.rennie6@gmail.com>
2018-12-23 14:39:16 +0200

    removed comma seperated list for production

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ const providers = ['google'];
 const callbacks = providers.map(provider =>
   process.env.NODE_ENV === 'production'
     ? // [
-      `http://localhost:4000/${provider}/callback,http://stockman-app-12345.herokuapp.com/${provider}/callback`
+      `http://stockman-app-12345.herokuapp.com/${provider}/callback`
     : //   `http://shallow-citadel-6j2x6vuw22eusm7mkcj10ief.herokudns.com/${provider}/callback`,
       //   `http://enigmatic-snake-inedbzvg3o8t7d89xzxpa54w.herokudns.com/${provider}/callback`
       `http://localhost:4000/${provider}/callback`
