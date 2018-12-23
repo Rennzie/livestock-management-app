82db4c6a1b7a9662620e094fb18b9237852f67be
Sean.Rennie <sean.rennie6@gmail.com>
2018-12-23 11:31:28 +0200

    reconfigured production and development URIs for google Oauth

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,19 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config();
+}
 
 const providers = ['google'];
 
 const callbacks = providers.map(provider =>
   process.env.NODE_ENV === 'production'
-    ? `http://react-auth-twitter.herokuapp.com/${provider}/callback`
+    ? [
+        `http://localhost:4000/${provider}/callback`,
+        `http://stockman-app-12345.herokuapp.com/${provider}/callback`,
+        `http://shallow-citadel-6j2x6vuw22eusm7mkcj10ief.herokudns.com/${provider}/callback`,
+        `http://enigmatic-snake-inedbzvg3o8t7d89xzxpa54w.herokudns.com/${provider}/callback`
+      ]
     : `http://localhost:4000/${provider}/callback`
 );
 
